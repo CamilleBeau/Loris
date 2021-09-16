3a83aeb8d637873590d617eefe699f2ca87bd445
jesscall <jessica.callegaro@mcin.ca>
2021-09-16 11:04:47 -0400

    added project to config and cand param, need to revamp sql structure

--- SQL/New_patches/2021-07-28_diagnosis_evolution.sql
+++ SQL/New_patches/2021-07-28_diagnosis_evolution.sql
@@ -1,13 +1,17 @@
 CREATE TABLE `diagnosis_evolution` (
   `DxEvolutionID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
+  `ProjectID` int(10) unsigned DEFAULT NULL,
   `visitLabel` varchar(255) DEFAULT NULL,
   `instrumentName` varchar(255) DEFAULT NULL,
   `sourceField` varchar(255) DEFAULT NULL,
   `orderNumber` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`DxEvolutionID`)
+  PRIMARY KEY (`DxEvolutionID`),
+  CONSTRAINT `FK_DxEvolution_1` FOREIGN KEY (`ProjectID`) REFERENCES `Project` (`ProjectID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- TODO: Remove this and replace with a dx_cand_rel table
+-- TODO: Update schema
 ALTER TABLE `candidate`
   ADD COLUMN `LatestDiagnosis` text DEFAULT NULL,
   ADD COLUMN `SourcedFromDxEvolutionID` int(10) unsigned DEFAULT NULL,
