0d66cea4f48c42b13914f65c14c79412e44cacaa
jesscall <jessica.callegaro@mcin.ca>
2022-06-07 13:13:01 -0400

    changing db initialization

--- modules/candidate_parameters/ajax/getData.php
+++ modules/candidate_parameters/ajax/getData.php
@@ -564,7 +564,7 @@ function getDODFields(): array
 function getDiagnosisEvolutionFields(): array
 {
     $candID = new CandID($_GET['candID']);
-    $db     = \Database::singleton();
+    $db     = \NDB_Factory::singleton()->database();
 
     $pscid = $db->pselectOne(
         "SELECT PSCID FROM candidate
