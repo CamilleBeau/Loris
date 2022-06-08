e4ee066b37003d3feea03ee68e3c667e6e72b154
Zaliqa <zaliqa.rosli@mcin.ca>
2022-06-08 16:35:28 -0400

    Update modules/candidate_parameters/ajax/getData.php

--- modules/candidate_parameters/ajax/getData.php
+++ modules/candidate_parameters/ajax/getData.php
@@ -603,7 +603,7 @@ function getDiagnosisEvolutionFields(): array
     $candidate       = \Candidate::singleton($candID);
     $latestDiagnosis = [];
     $latestConfirmedDiagnosis = [];
-    foreach ($candProj as $key => $projectID) {
+    foreach ($candProj as $projectID) {
         $latestDiagnosis[]          = $candidate->getLatestDiagnosis(
             new \ProjectID($projectID),
             false
