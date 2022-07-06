cb8a04bba1d2e2a04368ded18a46b6c965554934
zaliqarosli <zaliqa.rosli@mcin.ca>
2022-07-06 17:47:37 -0400

    fix null checks

--- tools/data_integrity/update_candidate_latest_diagnosis.php
+++ tools/data_integrity/update_candidate_latest_diagnosis.php
@@ -91,10 +91,7 @@ foreach ($candIDs as $k => $candID) {
                 $diagnosis    = [];
                 $sourceFields = explode(",", $data['sourceField']);
                 foreach ($sourceFields as $k => $fieldName) {
-                    if (!isset($instrumentData[$fieldName])
-                        || (isset($instrumentData[$fieldName])
-                        && is_null($instrumentData[$fieldName]))
-                    ) {
+                    if (!isset($instrumentData[$fieldName])) {
                         continue;
                     }
                     $diagnosis[$fieldName] = $instrumentData[$fieldName];
