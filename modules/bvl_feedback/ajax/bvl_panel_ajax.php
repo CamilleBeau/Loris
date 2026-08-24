0c438240bae8266da5cb88a319da718fcde740f9
CamilleBeau <camille.beaudoin@mcin.ca>
2026-08-24 08:53:07 -0400

    [bvl_feedback] Add candidate isAccessibleBy

--- modules/bvl_feedback/ajax/bvl_panel_ajax.php
+++ modules/bvl_feedback/ajax/bvl_panel_ajax.php
@@ -19,21 +19,26 @@
 namespace LORIS\bvl_feedback;
 use \LORIS\StudyEntities\Candidate\CandID;
 
-$user = \User::singleton();
-if (!$user->hasPermission('bvl_feedback')) {
-    header("HTTP/1.1 403 Forbidden");
-    header("Content-Type: text/plain");
-    exit(
-        "You do not have valid permissions for this
-         operation."
-    );
-}
-
-$username = $user->getUsername();
-$data     = \Utility::parseFormData($_POST);
+$data = \Utility::parseFormData($_POST);
 
 if (isset($data['candID']) && !empty($data['candID'])) {
     $candID    = new CandID($data['candID']);
+    $user      = \User::singleton();
+    $candidate = \Candidate::singleton($candID);
+
+    if (!$user->hasPermission('bvl_feedback')
+        || !$candidate->isAccessibleBy($user)
+    ) {
+        header("HTTP/1.1 403 Forbidden");
+        header("Content-Type: text/plain");
+        exit(
+            "You do not have valid permissions for this
+            operation."
+        );
+    }
+
+    $username = $user->getUsername();
+
     $sessionID = null;
     $commentID = null;
 
@@ -51,5 +56,13 @@ if (isset($data['candID']) && !empty($data['candID'])) {
         $sessionID,
         $commentID
     );
+} else {
+    if (!$user->hasPermission('bvl_feedback')) {
+        header("HTTP/1.1 400 Bad Request");
+        header("Content-Type: text/plain");
+        exit(
+            "A candID must be supplied for this operation."
+        );
+    }
 }
 
