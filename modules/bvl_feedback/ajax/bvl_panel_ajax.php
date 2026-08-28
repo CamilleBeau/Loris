57755bf421fe2a373776fdf4e3c714d326ce394f
CamilleBeau <camille.beaudoin@mcin.ca>
2026-08-28 13:50:37 -0400

    Dave's feedback

--- modules/bvl_feedback/ajax/bvl_panel_ajax.php
+++ modules/bvl_feedback/ajax/bvl_panel_ajax.php
@@ -19,16 +19,25 @@
 namespace LORIS\bvl_feedback;
 use \LORIS\StudyEntities\Candidate\CandID;
 
+$user = \User::singleton();
+if (!$user->hasPermission('bvl_feedback')) {
+    header("HTTP/1.1 403 Forbidden");
+    header("Content-Type: text/plain");
+    exit(
+        "You do not have valid permissions for this
+         operation."
+    );
+}
+
+$username = $user->getUsername();
+
 $data = \Utility::parseFormData($_POST);
 
 if (isset($data['candID']) && !empty($data['candID'])) {
     $candID    = new CandID($data['candID']);
-    $user      = \User::singleton();
     $candidate = \Candidate::singleton($candID);
 
-    if (!$user->hasPermission('bvl_feedback')
-        || !$candidate->isAccessibleBy($user)
-    ) {
+    if (!$candidate->isAccessibleBy($user)) {
         header("HTTP/1.1 403 Forbidden");
         header("Content-Type: text/plain");
         exit(
@@ -36,8 +45,6 @@ if (isset($data['candID']) && !empty($data['candID'])) {
             operation."
         );
     }
-
-    $username = $user->getUsername();
 
     $sessionID = null;
     $commentID = null;
